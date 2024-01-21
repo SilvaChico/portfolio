509be593e2d052465b6e9e3a41f31267ed0a4bb5
Francisco Silva <franciscocrosilva@gmail.com>
2024-01-21 19:05:46 +0000

    Fix SVG stroke-width attribute in Contacts component and update metadata in layout component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,62 +20,54 @@ export default function Home() {
           <div>
             <div className="hidden lg:flex lg:flex-row">
               <MyCanvas />
-              <div className="flex flex-col justify-center">
-                <h2 className="text-base font-bold text-slate-50">ABOUT</h2>
-                <h2 className="text-base font-bold text-slate-50">
-                  EXPERIENCE
-                </h2>
-                <h2 className="text-base font-bold text-slate-50">EDUCATION</h2>
-              </div>
             </div>
           </div>
           <Contacts />
         </div>
         <div className="flex flex-col lg:w-1/2 items-start max-h-screen text-white py-12 rounded-lg shadow-lg transition-colors overflow-y-auto scrollbar-hide">
           <div className="mb-16">
-            <h1 className="sticky top-0 z-20 -mx-6 mb-4 bg-gradient-to-r from-slate-900 to-bg-gray-900 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
-              ABOUT
-            </h1>
+            <h1 className="text-xl font-bold text-slate-50 mb-12">ABOUT</h1>
             <About />
           </div>
           <div className="mb-10 flex flex-col">
-            <h1 className="lg:hidden text-xl font-bold text-slate-50 mb-12">
+            <h1 className="text-xl font-bold text-slate-50 mb-12">
               EXPERIENCE
             </h1>
             <ExperienceCard
-              companyName="iCapital Network"
+              companyName="iCapital"
               description="Worked as a full stack engineer on the investment Dashboard building new features and improving existing ones. This dashboard is used by advisors to manage their clients' investments."
-              period="Dec 2021 - Present"
+              period="2021 - PRESENT"
               technologies={[
                 "React",
                 "Redux",
                 "Ruby on Rails",
                 "PostgreSQL",
                 "AWS",
+                "node.js",
               ]}
             />
             <ExperienceCard
               companyName="The Strategy Group"
               description="Built a Minimum Viable Product (MVP) of an AI chat system. This system is capable of integrating documents into its context, which means it can understand and respond to user queries based on the information contained in the uploaded documents."
-              period="Jun 2023 - Oct 2023 (side project)"
+              period="2023 (side project)"
               technologies={["Python", "LLM", "Llamaindex", "Streamlit"]}
             />
             <ExperienceCard
               companyName="SIBS"
               description="Backend engineer on the development of the central system of Portuguess ATMs and bank transactions."
-              period="May 2021 - Nov 2021"
+              period="2021"
               technologies={["Java"]}
             />
             <ExperienceCard
               companyName="Mobileum"
               description="Developed bespoke revenue assurance web applications for telecommunications providers utilizing our company's internal Low-Code tool. These applications successfully identified and mitigated revenue loss for millions of prepaid customers from companies like MTN Nigeria and Entel Chile."
-              period="Jan 2018 - Aug 2020"
+              period="2018 - 2020"
               technologies={["Java", "POSTGRESQL", "Oracle SQL"]}
             />
             <ExperienceCard
               companyName="UNINOVA"
               description="Part-time researcher/developer on the development of a sentiment analysis (using IBM's Watson) web app using social network data."
-              period="Nov 2016 - Jan 2018"
+              period="2016 - 2018"
               technologies={[
                 "JavaScript",
                 "Java",
@@ -92,9 +84,7 @@ export default function Home() {
             />
           </div>
           <div className="mb-10 flex flex-col">
-            <h1 className="lg:hidden text-xl font-bold text-slate-50 mb-12">
-              EDUCATION
-            </h1>
+            <h1 className="text-xl font-bold text-slate-50 mb-12">EDUCATION</h1>
             <EducationCard
               universityName="Faculdade de Ciências e Tecnologia da Universidade Nova de Lisboa"
               degree="Post-graduation in Electrical and Computer Engineering. Completed all master's degree courses with a 15/20 average."
--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,23 +2,24 @@ export default function About() {
   return (
     <div className="flex flex-col gap-3">
       <p>
-        Full Stack Engineer with over 6 years of experience in software
-        development, with a strong focus on web development.
+        As an experienced Full Stack Engineer with over 6 years of professional
+        experience, I specialize in web development and have a proven track
+        record of delivering high-quality software solutions. My journey in the
+        tech industry has led me to work on diverse projects, ranging from
+        developing a sentiment analysis web application using social network
+        data to building an innovative investment dashboard for iCapital, where
+        I currently contribute my expertise.
       </p>
       <p>
-        I have built a sentiment analysis web app using social network data, and
-        I am currently working as a Full Stack Engineer at iCapital. In my
-        current role, I am responsible for the development of an innovative
-        investment dashboard offering multi-fund insights to users.
+        One of my recent accomplishments includes collaborating with a fellow
+        engineer to develop an MVP for an AI chat system. This system stands out
+        for its ability to integrate documents into its context, thereby
+        enhancing its understanding and response to user queries.
       </p>
       <p>
-        Recently, I have collaborated with another software engineer to create
-        an MVP of an AI chat system capable of integrating documents into its
-        context.
-      </p>
-      <p>
-        My experience and skills make me a valuable asset to any team looking
-        for a talented Full Stack Engineer.
+        My technical skill set, coupled with my passion for problem-solving and
+        team collaboration, makes me a valuable asset to any team seeking a
+        dedicated and skilled Full Stack Engineer.
       </p>
     </div>
   );
